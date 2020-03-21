c71a09767ce2531906a2fb594a23bca1b8566c1d
Anthony <aventu6@uic.edu>
2020-03-21 18:49:07 -0500

    Cleaned Up Button code
    Made it  more professional and understandable

--- Arduino/Button/Button.ino
+++ Arduino/Button/Button.ino
@@ -1,10 +1,44 @@
-//signal comes in through pin 13 writes to serial
+/** HEADER *******************************************************************
+ * Author: Anthony Venturella                                                *
+ * GitHub: https://github.com/AnthonyVenturella                              *
+ *---------------------------------------------------------------------------*/
+/*  Description                                                              *
+ *  Button:                                                                  *
+ *    Program sends an integer to Serial when a button is pressed.           *
+ *    Default button pin is pin 13, default integer sent to Serial is 1      *
+ *---------------------------------------------------------------------------*/
+/*  Expected Board Layout:                                                   *
+ *                                                                           *
+ *  +--------------------------------------------+                           *
+ *  |   +-+           +---+                      |                           *
+ *  | +-|P|-----------|USB|---+                  |                           *
+ *  | | |W|           +---+   |                  |                           *
+ *  | | |R|                   |                  |                           *
+ *  | | +-+            AREF[ ]|                  |                           *
+ *  | |                 GND[ ]|                  |                           *
+ *  | |[ ]IOREF   LED[+] 13[-]|--]==[-[220ohm]-+ |                           *
+ *  | |[ ]RESET          12[ ]|  |SW|          | |                           *
+ *  | |[ ]3.3V           11[ ]|  ]==[------------+                           *
+ *  +-|[+]5V             10[ ]|                |                             *
+ * +--|[-]GND             9[ ]|                |                             *
+ * |  ~                       ~                |                             *
+ * |  |[ ]A3              2[ ]|                |                             *
+ * |  |[ ]A4              1[ ]|                |                             *
+ * |  |[ ]A5              0[ ]|                |                             *
+ * |  |                 ______/                |                             *
+ * |   \_______________/                       |                             *
+ * |                                           |                             *
+ * +-------------------------------------------+                             *
+ *                                                                           *
+ *****************************************************************************/
 
-int Button = 13;
+//Default values for button input and value to send to Serial
+static int Button = 13;
+static int SendVal = 1;
 
-int val1;
-int val2;
-int buttonState;
+int val1;         //for reading in a button pres
+int val2;         //for comparing in a debounce
+int buttonState;  //stores button state so 1 press = 1 press
 
 void setup() {
   pinMode(Button, INPUT);
@@ -13,6 +47,8 @@ void setup() {
 }
 
 void loop() {
+
+  //Brooklyn Debounce
   val1 = digitalRead(Button);
   delay(10);
   val2 = digitalRead(Button);
@@ -20,9 +56,10 @@ void loop() {
   if(val1 == val2){
     if(val1 != buttonState){
       if(val1 == HIGH){
-        Serial.println(1);
+        Serial.println(SendVal);
       }
     }
+    //storing button state for next loop check. Makes sure only reads once
     buttonState = val1;
   }
 }
